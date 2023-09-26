7971d76f379e54d12e191918695083962840a6cc
sbodrero <bodrero.sebastien@gmail.com>
2023-09-26 11:45:30 +0200

    Cleaned

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,11 +2,6 @@ import Link from './Link';
 
 function Sidebar() {
   const links = [
-    { label: 'Dropdown', path: '/' },
-    { label: 'Accordion', path: '/accordion' },
-    { label: 'Buttons', path: '/buttons' },
-    { label: 'Modal', path: '/modal' },
-    { label: 'Table', path: '/table' },
     { label: 'Counter', path: '/counter' },
   ];
 
