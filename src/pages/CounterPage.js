d15b7726ba889c9b27c95b44686bc4685e921088
sbodrero <bodrero.sebastien@gmail.com>
2023-09-28 10:51:04 +0200

    refactored to constants

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -2,26 +2,33 @@ import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
 
+const CounterPageConstants =  {
+  'INCREMENT': 'increment',
+  'DECREMENT': 'decrement',
+  'CHANGE_VALUE_TO_ADD': 'change_value_to_add',
+  'SUBMIT_FORM': 'submit_form',
+};
+
 const reducer = (state, action) => {
-  if (action.type === 'increment') {
+  if (action.type === CounterPageConstants.INCREMENT) {
     return {
       ...state,
       count: state.count + 1
     }
   }
-  if (action.type === 'decrement') {
+  if (action.type === CounterPageConstants.DECREMENT) {
     return {
       ...state,
       count: state.count - 1
     }
   }
-  if (action.type === 'change-value-to-add') {
+  if (action.type === CounterPageConstants.CHANGE_VALUE_TO_ADD) {
     return {
       ...state,
       valueToAdd: action.payload
     }
   }
-  if (action.type = 'submit-form') {
+  if (action.type = CounterPageConstants.SUBMIT_FORM) {
     return {
       ...state,
       count: state.count + state.valueToAdd,
@@ -43,20 +50,20 @@ function CounterPage({ initialCount }) {
   const increment = () => {
     // setCount(prev => prev + 1);
     dispatch({
-      type: 'increment',
+      type: CounterPageConstants.INCREMENT,
     });
   };
   const decrement = () => {
     // setCount(prev => prev - 1);
     dispatch({
-      type: 'decrement',
+      type: CounterPageConstants.DECREMENT,
     });
   };
 
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
     dispatch({
-      type: 'change-value-to-add',
+      type: CounterPageConstants.CHANGE_VALUE_TO_ADD,
       payload: value,
     })
     // setValueToAdd(value);
@@ -65,7 +72,7 @@ function CounterPage({ initialCount }) {
   const handleSubmit = (event) => {
     event.preventDefault();
     dispatch({
-      type: 'submit-form',
+      type: CounterPageConstants.SUBMIT_FORM,
     })
     // setCount(prev => prev + valueToAdd);
     // setValueToAdd(0);
